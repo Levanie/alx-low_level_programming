385fb51fa11c1644367d60e2e404e3dc2cc3a000
Levanie <evahmugo690@gmail.com>
2022-12-22 13:12:01 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,32 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words
- * @s: String to be capitalized
- * Return: string.
+ * cap_string - capitalizes all words in a string
+ * @s: string
+ * Return: address of s
  */
 char *cap_string(char *s)
 {
-	int i = 1;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	if (s[0] >= 'a' && s[0] <= 'z')
+	while (*(s + i))
 	{
-		s[0] = s[0] - 32;
-	}
-	for (; s[i]; i++)
-	{
-		if ((s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t' || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}' || s[i - 1] == '.') && (s[i] > 'a' && s[i] < 'z'))
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			s[i] = s[i] - 32;
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
 		}
+		i++;
+
 	}
 	return (s);
 }
